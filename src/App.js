6264b63142e15909a75b25c82de465ec2ce27a32
takwa melki <takwamelki484@gmail.com>
2021-05-01 14:26:34 +0100

    adding commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import myImage from "./success.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="menu">
+      <div style = {{border: "1px solid black", maxWidth: "100vw"}} className = "container">
+        <h1 className = "title red">Perpetual optimism is a force multiplier !</h1>
+        <br/>
+        <img src = {myImage}   alt = 'push-your-self'  height = '450px'   width = '700px'/>
+        <br/>
+        <img src = "/self-confidence.jpg"  alt = "believe in your self"  height = "450px"   width = "700px"/>
+      </div>
+      <video width = "320"  height = "240"  controls>
+        <source src = "/video.mp4"   type = "video/mp4" />
+      </video>
     </div>
   );
 }
